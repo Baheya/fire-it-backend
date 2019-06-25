a938e2a16a22566274eef17656d390c441fcee00
Baheya Khalifa <beya@Baheyas-MacBook-Pro.local>
2019-06-25 18:43:03 +0200

    initial commit

---
+++ controllers/feed.js
@@ -0,0 +1,29 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "Hello",
+        imageUrl: "images/Mountain.jpeg",
+        author: "Baheya",
+        createdAt: new Date()
+      }
+    ]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      author: { name: "Baheya" },
+      createdAt: new Date()
+    }
+  });
+};
